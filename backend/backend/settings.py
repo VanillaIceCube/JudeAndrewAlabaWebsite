1189f8f435d898894988357ed165cd289cb5508b
IceCobe <jaalaba@gmail.com>
2024-07-08 15:54:38 -0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+
 import os
+import sys
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
