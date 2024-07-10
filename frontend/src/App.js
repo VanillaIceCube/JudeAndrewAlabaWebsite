d30211db019d68db624771f7f49f0ff59ebcf3e3
IceCobe <jaalaba@gmail.com>
2024-07-09 18:00:27 -0700

    react-router-dom added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  console.log("Hello World")
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +26,42 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>About Page</h1>
+        <p>This is the about page.</p>
+        <Link to="/">Go back to Home</Link>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  console.log("Hello World")
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
