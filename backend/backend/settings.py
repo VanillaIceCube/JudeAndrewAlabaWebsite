baa7d69c8862d0fc6c2a9bfcc031320e17b01515
IceCobe <jaalaba@gmail.com>
2024-07-09 17:43:43 -0700

    refactor settings.py & urls.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,17 +48,18 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
+
 CORS_ALLOWED_ORIGINS = [
-    "https://jude-andrew-alaba-peaab.ondigitalocean.app/",
+    "https://jude-andrew-alaba-peaab.ondigitalocean.app",
 ]
 
 ROOT_URLCONF = "backend.urls"
