e56dd40292e6e0debc8ec9cabf6c8eb7f5180b32
IceCobe <jaalaba@gmail.com>
2024-07-09 19:00:44 -0700

    test_world

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from views import hello_world
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
+    path("test/", hello_world)
 ]
