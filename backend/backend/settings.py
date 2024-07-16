233a71e6c9251f6aeba530357634f27f9ba6db51
IceCobe <jaalaba@gmail.com>
2024-07-16 16:08:15 -0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,5 +147,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-LOGIN_URL = 'api/admin/login/'
