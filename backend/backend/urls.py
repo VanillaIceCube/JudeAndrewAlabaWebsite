30f713fd4cd4afe500f95ed97cab5f00dd5c1169
IceCobe <jaalaba@gmail.com>
2024-07-16 15:05:53 -0700

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.urls import path
 
 urlpatterns = [
-    path('', include('attendance.urls')),
-    path('admin/', admin.site.urls),
-
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
+    path("admin/", admin.site.urls),
 ]
