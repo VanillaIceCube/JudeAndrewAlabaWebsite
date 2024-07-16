90eee1b656ab948998e42e151cfeb2313676bf16
IceCobe <jaalaba@gmail.com>
2024-07-16 15:42:57 -0700

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("api/admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 # Debugging: Print the registered URL patterns
