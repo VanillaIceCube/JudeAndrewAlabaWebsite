dc9c866bbef3648ac3a451336a8459c5418779fc
IceCobe <jaalaba@gmail.com>
2024-07-17 14:52:07 -0700

    refactor: full app refactor

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,16 +1,6 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
+from django.core.asgi import get_asgi_application
 import os
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
 application = get_asgi_application()
