890656a95b5b88a9f125ea8d1f178c3e2a2125f2
IceCobe <jaalaba@gmail.com>
2024-07-23 15:37:29 -0700

    Update MyToolbar.js

--- frontend/src/components/MyToolbar.js
+++ frontend/src/components/MyToolbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 const MyToolbar = () => {
