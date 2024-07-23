38efbbbfb607d31a2a95bd5264092c02698c7064
IceCobe <jaalaba@gmail.com>
2024-07-23 15:40:18 -0700

    template spice up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,30 @@
+// src/App.js
 import React from 'react';
 import MyToolbar from './components/MyToolbar';
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 const App = () => {
   return (
     <div>
       <MyToolbar />
-      <div style={{ padding: 20 }}>
-        <Typography variant="body1">
-          Hello this is my Website!
-        </Typography>
-      </div>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Paper elevation={3} sx={{ p: 4 }}>
+          <Typography variant="h4" gutterBottom>
+            Welcome to My Website
+          </Typography>
+          <Typography variant="body1" paragraph>
+            This is a sample website using Material-UI. The toolbar at the top contains three buttons, each with an icon.
+          </Typography>
+          <Box sx={{ mt: 2, display: 'flex', justifyContent: 'center' }}>
+            <Typography variant="body1" sx={{ maxWidth: '600px', textAlign: 'center' }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nibh eget suscipit tincidunt, nunc odio malesuada justo, non dapibus leo felis ut quam. Aliquam erat volutpat.
+            </Typography>
+          </Box>
+        </Paper>
+      </Container>
     </div>
   );
 };
--- frontend/src/components/MyToolbar.js
+++ frontend/src/components/MyToolbar.js
@@ -1,17 +1,21 @@
+// src/components/MyToolbar.js
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
 
 const MyToolbar = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}>
       <Toolbar>
         <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-          <Button color="inherit">Button 1</Button>
-          <Button color="inherit">Button 2</Button>
-          <Button color="inherit">Button 3</Button>
+          <Button color="inherit" startIcon={<HomeIcon />} sx={{ mx: 1 }}>Home</Button>
+          <Button color="inherit" startIcon={<InfoIcon />} sx={{ mx: 1 }}>About</Button>
+          <Button color="inherit" startIcon={<ContactMailIcon />} sx={{ mx: 1 }}>Contact</Button>
         </Box>
       </Toolbar>
     </AppBar>
