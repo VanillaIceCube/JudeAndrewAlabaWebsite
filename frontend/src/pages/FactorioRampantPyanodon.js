cc5b53025e9fe18d6299c3c321f9e4fecab0f863
IceCobe <jaalaba@gmail.com>
2024-07-25 15:44:13 -0700

    Embeded my latest basetour episode!

--- frontend/src/pages/FactorioRampantPyanodon.js
+++ frontend/src/pages/FactorioRampantPyanodon.js
@@ -12,13 +12,27 @@ const FactorioRampantPyanodon = () => (
         Factorio Rampant Pyanodon
       </Typography>
       <Box sx={{ mt: 2 }}>
-        <Typography variant="body1">
-          Hehe, I'm gonna plug my {' '}
+        <Typography variant="body1" gutterBottom>
+          Hehe, I'm gonna plug my{' '}
           <Link href="https://www.youtube.com/@VanillaIceCube" target="_blank" rel="noopener noreferrer">
-            youtube channel
-          </Link>
-          .
+            YouTube channel
+          </Link>.
         </Typography>
+        <Typography variant="body1" gutterBottom>
+          Here's my latest basetour episode!
+        </Typography>
+        <Box sx={{ mt: 4, position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+          <iframe
+            title="Factorio Rampant Pyanodon Video"
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/XCvq1H2OVWs?si=1U_alQOuaZOev2ap"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+          />
+        </Box>
       </Box>
     </Paper>
   </Container>
