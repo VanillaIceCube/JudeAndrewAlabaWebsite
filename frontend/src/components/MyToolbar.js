0301c504830a0ca315a5d298d36b4d3debab05a1
IceCobe <jaalaba@gmail.com>
2024-07-30 15:42:40 -0700

    added: MyToolbar underline & animation

--- frontend/src/components/MyToolbar.js
+++ frontend/src/components/MyToolbar.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
+import React, { useState, useEffect, useRef } from 'react';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import RouteIcon from '@mui/icons-material/Route';
 import PersonIcon from '@mui/icons-material/Person';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './MyToolbar.css';
 
 const MyToolbar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const [underlineStyle, setUnderlineStyle] = useState({});
+  const buttonsRef = useRef([]);
+
+  useEffect(() => {
+    const activeButton = buttonsRef.current.find(button => button && button.dataset.path === currentPath);
+    if (activeButton) {
+      const { offsetLeft, offsetWidth } = activeButton;
+      setUnderlineStyle({
+        left: offsetLeft,
+        width: offsetWidth,
+      });
+    }
+  }, [currentPath]);
+
   return (
     <AppBar position="static" className="toolbar">
       <Toolbar>
-        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', position: 'relative' }}>
           <Button
             component={Link}
             to="/about-me"
             color="inherit"
             startIcon={<PersonIcon />}
+            className={currentPath === '/about-me' ? 'active' : ''}
             sx={{ mx: 1 }}
+            data-path="/about-me"
+            ref={el => buttonsRef.current[0] = el}
           >
             About me
           </Button>
@@ -28,7 +44,10 @@ const MyToolbar = () => {
             to="/data-engineering-project"
             color="inherit"
             startIcon={<RouteIcon />}
+            className={currentPath === '/data-engineering-project' ? 'active' : ''}
             sx={{ mx: 1 }}
+            data-path="/data-engineering-project"
+            ref={el => buttonsRef.current[1] = el}
           >
             Data Engineering Project
           </Button>
@@ -37,10 +56,14 @@ const MyToolbar = () => {
             to="/factorio-rampant-pyanodon"
             color="inherit"
             startIcon={<YouTubeIcon />}
+            className={currentPath === '/factorio-rampant-pyanodon' ? 'active' : ''}
             sx={{ mx: 1 }}
+            data-path="/factorio-rampant-pyanodon"
+            ref={el => buttonsRef.current[2] = el}
           >
             Factorio Rampant Pyanodon
           </Button>
+          <div className="underline" style={underlineStyle}></div>
         </Box>
       </Toolbar>
     </AppBar>
