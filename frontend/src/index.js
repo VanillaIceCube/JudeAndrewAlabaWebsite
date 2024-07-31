b0628d44ebd796ad5a241819b4c6d5bac41f583c
IceCobe <jaalaba@gmail.com>
2024-07-31 10:36:16 -0700

    add: Hi I'm Jude.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Dynamically add the Google Fonts link
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
--- frontend/src/pages/AboutMe.js
+++ frontend/src/pages/AboutMe.js
@@ -3,42 +3,32 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Avatar from '@mui/material/Avatar';
 import Divider from '@mui/material/Divider';
+import './AboutMe.css';
 
 const AboutMe = () => (
   <Container maxWidth="md" sx={{ mt: 4 }}>
     <Paper elevation={3} sx={{ p: 4, backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
-      <Typography variant="h4" gutterBottom>
-        About Me
-      </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={12} sm={4}>
-          <Avatar 
-            alt="Profile Picture" 
-            src="https://media.licdn.com/dms/image/D5603AQHQ4u6GR83ZSA/profile-displayphoto-shrink_200_200/0/1706633212465?e=1727913600&v=beta&t=ObBTdTUGC0fZ14X-aIjLnNWQ-kfKri7ailBwXYS_0WA" 
-            sx={{ width: 150, height: 150, mb: 2, mx: 'auto' }} 
-          />
-        </Grid>
-        <Grid item xs={12} sm={8}>
-          <Typography variant="body1" gutterBottom>
-            Hello! My name is Jude Andrew Alaba, and I made this website. I'm not quite sure what to put here yet, but I do know that I would like to show, not tell.
+      <div className="image-container">
+      <img src={`${process.env.PUBLIC_URL}/Florence.jpg`} alt="Background" />
+        <div className="overlay">
+          <Typography variant="h2" gutterBottom className="jude-text">
+            Hi! I'm Jude.
           </Typography>
-        </Grid>
-      </Grid>
-      
+        </div>
+      </div>
+
       <Divider sx={{ my: 4 }} />
-      
+
       <Typography variant="h5" gutterBottom>
         Biography
       </Typography>
       <Typography variant="body1" gutterBottom>
         A long long time ago, in a country far away...
       </Typography>
-      
+
       <Divider sx={{ my: 4 }} />
-      
+
       <Typography variant="h5" gutterBottom>
         Hobbies
       </Typography>
